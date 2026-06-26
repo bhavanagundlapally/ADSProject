5d8bc7fd64162aa221662a4aa9d0b2040f5606a8
bhavanagundlapally <bhavanagundlapally16@gmail.com>
2026-06-26 15:38:56 +0200

    Implemented forwarding unit

--- 04_forwarding_unit/src/main/scala/EXbarrier.scala
+++ 04_forwarding_unit/src/main/scala/EXbarrier.scala
@@ -26,13 +26,57 @@ Functionality:
     Save all input signals to a register and output them in the following clock cycle
 */
 
+// ADS I Class Project
+// Pipelined RISC-V Core - EX Barrier
+//
+// Chair of Electronic Design Automation, RPTU in Kaiserslautern
+// File created on 01/09/2026 by Tobias Jauch (@tojauch)
+
+/*
+EX-Barrier: pipeline register between Execute and Memory stages
+
+Internal Registers:
+    aluResult: ALU computation result
+    rd: destination register index
+    exception: exception flag
+
+Inputs:
+    inAluResult: computation result from EX stage
+    inRD: destination register from EX stage
+    inXcptInvalid: exception flag from EX stage
+
+Outputs:
+    outAluResult: result to MEM stage
+    outRD: destination register to MEM stage
+    outXcptInvalid: exception flag to MEM stage
+
+Functionality:
+    Save all input signals to a register and output them in the following clock cycle
+*/
+
 package core_tile
 
 import chisel3._
+import chisel3.util._
 
-// -----------------------------------------
-// EX-Barrier
-// -----------------------------------------
+// -------------------------------------------------------------
+// EX to MEM Barrier Register
+// -------------------------------------------------------------
+class EXBarrier extends Module {
+  val io = IO(new Bundle {
+    val inAluResult   = Input(UInt(32.W))
+    val inRD          = Input(UInt(5.W))
+    val inXcptInvalid = Input(Bool())
+    val inRegWrite    = Input(Bool())
 
-//ToDo: Add your implementation according to the specification above here 
+    val outAluResult  = Output(UInt(32.W))
+    val outRD         = Output(UInt(5.W))
+    val outXcptInvalid= Output(Bool())
+    val outRegWrite   = Output(Bool())
+  })
 
+  io.outAluResult   := RegNext(io.inAluResult)
+  io.outRD          := RegNext(io.inRD)
+  io.outXcptInvalid := RegNext(io.inXcptInvalid)
+  io.outRegWrite    := RegNext(io.inRegWrite, false.B)
+}
